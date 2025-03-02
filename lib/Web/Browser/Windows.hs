7f0fde3e76ffa9ff18f301cb40db157f31085dfa
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-03-02 17:16:52 +0000

    Reformat existing code and undate its Haddock documentation

--- lib/Web/Browser/Windows.hs
+++ lib/Web/Browser/Windows.hs
@@ -1,31 +1,35 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
-module Web.Browser.Windows
-( openBrowserWindows
-) where
 
-import System.Win32.Types (INT, HANDLE, HINSTANCE, LPCTSTR,
-                           handleToWord, nullPtr, withTString)
+module Web.Browser.Windows
+  ( openBrowserWindows
+  ) where
+
+import System.Win32.Types
+         ( HANDLE, HINSTANCE, INT, LPCTSTR, handleToWord, nullPtr, withTString )
 
 openBrowserWindows :: String -> IO Bool
 openBrowserWindows url =
-    withTString "open" $ \openStr ->
-        withTString url $ \urlStr ->
-            exitCodeToBool `fmap` c_ShellExecute nullPtr
-                                                 openStr
-                                                 urlStr
-                                                 nullPtr
-                                                 nullPtr
-                                                 1
-  where exitCodeToBool hinst | handleToWord hinst > 32 = True
-                             | otherwise               = False
+  withTString "open" $ \openStr ->
+    withTString url $ \urlStr ->
+      exitCodeToBool `fmap` c_ShellExecute nullPtr
+                                           openStr
+                                           urlStr
+                                           nullPtr
+                                           nullPtr
+                                           1
+ where
+  exitCodeToBool hinst
+    | handleToWord hinst > 32 = True
+    | otherwise               = False
 
--- https://msdn.microsoft.com/en-us/library/windows/desktop/bb762153(v=vs.85).aspx
+-- https://learn.microsoft.com/en-us/windows/win32/api/shellapi/nf-shellapi-shellexecutew
 foreign import WINDOWS_CCONV unsafe "windows.h ShellExecuteW"
-    c_ShellExecute :: HANDLE  -- _In_opt_
-                   -> LPCTSTR -- _In_opt_
-                   -> LPCTSTR -- _In_
-                   -> LPCTSTR -- _In_opt_
-                   -> LPCTSTR -- _In_opt_
-                   -> INT     -- _In_
-                   -> IO HINSTANCE
+  c_ShellExecute ::
+       HANDLE  -- _In_opt_
+    -> LPCTSTR -- _In_opt_
+    -> LPCTSTR -- _In_
+    -> LPCTSTR -- _In_opt_
+    -> LPCTSTR -- _In_opt_
+    -> INT     -- _In_
+    -> IO HINSTANCE
