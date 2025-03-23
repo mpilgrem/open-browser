bc17a239e1699c053de92a227e3f04abf8c17452
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2025-03-23 17:35:01 +0000

    Fix #1 Use Haskell to discard application output

--- lib/unix-like/common/Utils.hs
+++ lib/unix-like/common/Utils.hs
@@ -1,21 +1,30 @@
 --------------------------------------------------------------------------------
--- For operating systems that use an application or a script on the PATH to open
--- the URL.
+-- For Unix-like operating systems that use an application or a script on the
+-- PATH to open the URL.
 --------------------------------------------------------------------------------
 
 module Utils
-  ( openBrowserWith
+  ( openBrowserSilentlyWith
   ) where
 
 import System.Exit ( ExitCode (..) )
-import System.Process ( proc, waitForProcess, withCreateProcess )
+import System.IO ( IOMode (..), withFile )
+import System.Process
+         ( CreateProcess (..), StdStream (..), proc, waitForProcess
+         , withCreateProcess
+         )
 
-openBrowserWith ::
+-- | Discards all standard output and standard error from the process.
+openBrowserSilentlyWith ::
      String
      -- ^ Name of relevant executable on the PATH.
   -> [String]
      -- ^ Arguments for executable.
   -> IO Bool
-openBrowserWith cmd args =
-  withCreateProcess (proc cmd args) $ \_ _ _ p ->
-    (== ExitSuccess) <$> waitForProcess p
+openBrowserSilentlyWith cmd args =
+  -- /dev/null is specifc to Unix-like operating systems
+  withFile "/dev/null" WriteMode $ \nullHandle ->
+    withCreateProcess (proc cmd args)
+      { std_out = UseHandle nullHandle
+      , std_err = UseHandle nullHandle
+      } $ \_ _ _ p -> (== ExitSuccess) <$> waitForProcess p
